d1fbe9f4c068b317649e6452f1ec50bc56edc5b7
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2023-11-22 18:15:24 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 124 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static final Integer DIFFERENCE = 543;
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        Integer y = Integer.parseInt(st.nextToken());
+
+        bw.write(Integer.toString(y - DIFFERENCE));
+
+        bf.close();
+        bw.flush();
+        bw.close();
+    }
+}
