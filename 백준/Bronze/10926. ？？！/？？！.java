5b047c7696821b61e6bb07e70aec2c12a37b388d
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2023-11-22 18:11:56 +0900

    [Bronze V] Title: ??!, Time: 128 ms, Memory: 14356 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        bw.write(st.nextToken() + "??!\n");
+
+        bf.close();
+        bw.flush();
+        bw.close();
+    }
+}
