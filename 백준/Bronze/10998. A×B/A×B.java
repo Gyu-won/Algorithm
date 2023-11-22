c9ba3ed10f98ef87b49c69a910f612433622c2e4
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2023-11-22 18:01:47 +0900

    [Bronze V] Title: A×B, Time: 128 ms, Memory: 14384 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        Integer A = Integer.parseInt(st.nextToken());
+        Integer B = Integer.parseInt(st.nextToken());
+
+        bw.write(Integer.toString(A*B));
+
+        bf.close();
+        bw.flush();
+        bw.close();
+    }
+}
