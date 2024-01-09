f647b8c296d36cf046471178027daab8a144a81d
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2024-01-10 01:20:32 +0900

    [Silver III] Title: 소수 구하기, Time: 476 ms, Memory: 19024 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = M; i <= N; i++) {
+            if (i > 1 && isPrime(i)) {
+                result.append(i);
+                result.append("\n");
+            }
+        }
+
+        System.out.println(result);
+    }
+
+    private static boolean isPrime(int number) {
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
