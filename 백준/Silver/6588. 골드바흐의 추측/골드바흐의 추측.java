3a5a74ee5402486e2be963ad73bf4023536f11e0
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2024-01-10 01:03:16 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 724 ms, Memory: 53272 KB -BaekjoonHub

---
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.java
@@ -0,0 +1,43 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    final static int MAX_NUMBER = 1000000;
+    static boolean[] notPrime = new boolean[MAX_NUMBER];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        findPrimes();
+
+        StringBuilder result = new StringBuilder();
+        while (true) {
+            String n = br.readLine();
+            if (n.equals("0")) {
+                break;
+            }
+
+            result.append(validGoldbach(Integer.parseInt(n)));
+            result.append("\n");
+        }
+        System.out.println(result);
+    }
+
+    private static String validGoldbach(int n) {
+        for (int odd = 3; odd <= n / 2; odd += 2) {
+            if (!notPrime[odd] && !notPrime[n - odd]) {
+                return String.valueOf(n) + " = " + String.valueOf(odd) + " + " + String.valueOf(n-odd);
+            }
+        }
+        return "Goldbach's conjecture is wrong.";
+    }
+
+    private static void findPrimes() {
+        for (int i = 3; i <= Math.sqrt(MAX_NUMBER); i += 2) {
+            for (int j = 3; i * j < MAX_NUMBER; j += 2) {
+                notPrime[i * j] = true;
+            }
+        }
+    }
+}
