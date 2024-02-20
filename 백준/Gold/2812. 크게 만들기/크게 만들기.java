1562315200db8a6c432016a094dba395aea40696
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2024-02-20 12:06:42 +0900

    [Gold III] Title: 크게 만들기, Time: 308 ms, Memory: 21208 KB -BaekjoonHub

--- 백준/Gold/2812. 크게 만들기/크게 만들기.java
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -21,20 +19,30 @@ public class Main {
             numbers[i] = number.charAt(i) - '0';
         }
 
-        List<Integer> sortedNumbers = removeSmallerNumber(n, k, numbers);
-
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < n - k; i++) {
-            result.append(sortedNumbers.get(i));
-        }
-        System.out.println(result.toString());
+        boolean[] removed = removeSmallerNumber(n, k, numbers);
+       
+        System.out.println(selectFrontNumbers(n, k, removed, numbers));
     }
 
-    private static List<Integer> removeSmallerNumber(int n, int k, int[] numbers) {
+    private static String selectFrontNumbers(int n, int k, boolean[] removed, int[] numbers) {
+        int length = 0;
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            if (length == n - k) {
+                break;
+            }
+            if (!removed[i]) {
+                result.append(numbers[i]);
+                length++;
+            }
+        }
+        return result.toString();
+    }
+
+    private static boolean[] removeSmallerNumber(int n, int k, int[] numbers) {
         boolean[] removed = new boolean[n];
 
         int count = 0;
-
         exit:
         for (int i = 1; i < n; i++) {
             for (int j = i - 1; j >= 0; j--) {
@@ -52,13 +60,7 @@ public class Main {
             }
         }
 
-        List<Integer> sortedNumbers = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            if (!removed[i]) {
-                sortedNumbers.add(numbers[i]);
-            }
-        }
-        return sortedNumbers;
+        return removed;
     }
 }
 
