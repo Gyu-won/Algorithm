61216b8c8341573aba9d2cc9ff605da6b02bd668
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2024-02-20 14:49:45 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,10 +1,17 @@
 -- 코드를 입력하세요
-SELECT i.name, i.datetime
-from animal_ins i
-    left join animal_outs o
-    on i.animal_id = o.animal_id
-where o.animal_id is null
-order by i.datetime
+# SELECT i.name, i.datetime
+# from animal_ins i
+#     left join animal_outs o
+#     on i.animal_id = o.animal_id
+# where o.animal_id is null
+# order by i.datetime
+# limit 3;
+
+SELECT a.name, a.datetime
+from animal_ins as a
+left join animal_outs as b on a.animal_id=b.animal_id
+where b.animal_id is NULL
+order by DATE_FORMAT(a.datetime,"%Y-%m-%d")
 limit 3;
 
 # 01:53
