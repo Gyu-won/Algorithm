41e28cc9e69f846e5d3a7cd1c3dcaaa1cec4171e
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2024-02-24 00:14:57 +0900

    [level 3] Title: 자동차 대여 기록에서 대여중 / 대여 가능 여부 구분하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
+++ 프로그래머스/3/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
@@ -1,33 +1,13 @@
--- 코드를 입력하세요
-# SELECT car_id, 
-#     case when date_format(start_date, '%Y-%m-%d') <= '2022-10-16' and
-#         date_format(end_date, '%Y-%m-%d') >= '2022-10-16' then '대여중'
-#     else '대여 가능'
-#     end as availability
-# from car_rental_company_rental_history
-# where 
-# order by car_id desc;
 
-
-select c.car_id, 
-    case when m.flag = 1 then '대여중'
-    else '대여 가능'
+select car_id,
+    case
+        when car_id in (select car_id
+                    from car_rental_company_rental_history
+                    where '2022-10-16' 
+                        between date_format(start_date, '%Y-%m-%d') and date_format(end_date, '%Y-%m-%d'))
+                        then '대여중'
+        else '대여 가능'
     end as availability
-from car_rental_company_rental_history c 
-join (
-    select car_id, 
-        max(case 
-            when date_format(start_date, '%Y-%m-%d') <= '2022-10-16' 
-                and date_format(end_date, '%Y-%m-%d') >= '2022-10-16' 
-            then 1
-            else 0
-        end) as flag
-    from car_rental_company_rental_history
-    group by car_id) m 
-on c.car_id = m.car_id
+from car_rental_company_rental_history
 group by car_id
 order by car_id desc;
-
-
-# 2022년 10월 16일 대여 중인 자동차는 대여중 아니면 대여가능 (availability)
-# 
