900028838754f60419b5cc6ee9320c9e76d37e56
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2024-02-24 00:32:08 +0900

    [level 4] Title: 저자 별 카테고리 별 매출액 집계하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
+++ 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
@@ -1,24 +1,12 @@
--- 코드를 입력하세요
-select book_data.author_id, book_data.author_name, book_data.category, 
-    sum(book_data.price * sale_data.sales) as total_sales
-from (select b.book_id, a.author_id, a.author_name, b.category, b.price
-    from book b
-    join author a
-    on b.author_id = a.author_id) book_data
+select book.author_id, author.author_name, book.category, 
+    sum(book_sales.sales * book.price) as sales
+from book
+    join author
+    on book.author_id = author.author_id
     
-    join (SELECT book_id, sum(sales) as sales
-        from book_sales
-        where date_format(sales_date, '%Y-%m') = '2022-01'
-        group by book_id) sale_data
-        
-    on book_data.book_id = sale_data.book_id
-group by book_data.author_id, book_data.category
-order by book_data.author_id, book_data.category desc;
-    
+    join book_sales
+    on book.book_id = book_sales.book_id
 
-
-
-#2022년 1 판매 된거
-# 저자별, 카테고리별 매출액
-
-#16:59 - 
+where date_format(book_sales.sales_date, '%Y-%m') = '2022-01'
+group by book.author_id, book.category
+order by book.author_id, book.category desc;
