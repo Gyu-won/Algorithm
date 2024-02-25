c2fe8a1892cb12978a0d5e83227cd367c3830eb0
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2024-02-25 16:57:52 +0900

    [Gold III] Title: 파티, Time: 204 ms, Memory: 17780 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Deque;
 import java.util.List;
 import java.util.StringTokenizer;
@@ -19,90 +20,38 @@ public class Main {
         int m = Integer.parseInt(st.nextToken());
         int x = Integer.parseInt(st.nextToken());
 
-//        // m개의 도로를 입력 (시작, 끝, t)
-//        List<Road>[] roads = new ArrayList[n + 1];
-//        for (int i = 1; i <= n; i++) {
-//            roads[i] = new ArrayList<>();
-//        }
-//
-//        for (int i = 0; i < m; i++) {
-//            st = new StringTokenizer(br.readLine(), " ");
-//            int start = Integer.parseInt(st.nextToken());
-//            int dest = Integer.parseInt(st.nextToken());
-//            int time = Integer.parseInt(st.nextToken());
-//
-//            roads[start].add(new Road(dest, time));
-//        }
-
-        int[][] distance = new int[n + 1][n + 1];
+        // m개의 도로를 입력 (시작, 끝, t)
+        List<Road>[] roads = new ArrayList[n + 1];
+        List<Road>[] reverseRoads = new ArrayList[n + 1];
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (i == j) {
-                    distance[i][j] = 0;
-                    continue;
-                }
-                distance[i][j] = INF;
-            }
+            roads[i] = new ArrayList<>();
+            reverseRoads[i] = new ArrayList<>();
         }
 
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine(), " ");
-            
             int start = Integer.parseInt(st.nextToken());
             int dest = Integer.parseInt(st.nextToken());
             int time = Integer.parseInt(st.nextToken());
-            distance[start][dest] = Math.min(distance[start][dest], time);
+
+            roads[start].add(new Road(dest, time));
+            reverseRoads[dest].add(new Road(start, time));
         }
 
-        for (int k = 1; k <= n; k++) {
-            for (int i = 1; i <= n; i++) {
-                for (int j = 1; j <= n; j++) {
-                    if (distance[i][k] + distance[k][j] < distance[i][j]) {
-                        distance[i][j] = distance[i][k] + distance[k][j];
-                    }
-                }
-            }
-        }
+        // 다익스트라로 (x에서 n으로 가는 경로 구하기) O(m)
+        int[] comeback = dijkstra(x, n, roads);
 
+        // bfs로 n -> x로 가는 경로 구하기, O(n * nlogn)
+        int[] go = dijkstra(x, n, reverseRoads);
+
+        //둘을 더해서 최대인 값을 리턴
         int maxValue = 0;
         for (int i = 1; i <= n; i++) {
-            if (i != x) {
-                maxValue = Math.max(distance[i][x] + distance[x][i], maxValue);
-            }
+            maxValue = Math.max(maxValue, go[i] + comeback[i]);
         }
 
         System.out.println(maxValue);
-
-//        // 다익스트라로 (x에서 n으로 가는 경로 구하기) O(m)
-//        int[] comeback = dijkstra(x, n, roads);
-//
-//        // bfs로 n -> x로 가는 경로 구하기, O(n * nlogn)
-//        int[] go = new int[n+1];
-//        for (int i = 1; i <= n; i++){
-//            if (i != x){
-//                go[i] = calculateMinDistance(i, x);
-//            }
-//        }
-
-        // 둘을 더해서 최대인 값을 리턴
     }
-
-//    private static int calculateMinDistance(int start, int dest){
-//        PriorityQueue<Road> pq = new PriorityQueue<>(new RoadComparator());
-//        pq.offer(new Road(start, 0));
-//
-//        while (true){
-//            Road road = pq.poll();
-//            if (road.dest == dest){
-//                return road.time;
-//            }
-//
-//            for (Road road: roads){
-//                pq.offer()
-//            }
-//
-//        }
-//    }
 
     private static int[] dijkstra(int x, int n, List<Road>[] roads) {
         int[] comeback = new int[n + 1];
