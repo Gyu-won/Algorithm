746b000ae8b604f830f5ff33ccc76ffa0ba92272
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2024-02-25 16:47:38 +0900

    [Gold III] Title: 파티, Time: 2440 ms, Memory: 30860 KB -BaekjoonHub

---
+++ 백준/Gold/1238. 파티/파티.java
@@ -0,0 +1,146 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static final int INF = 1000000;
+
+    public static void main(String[] args) throws IOException {
+        // n, m, x를 입력 (1-1000), (1-10000)
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int x = Integer.parseInt(st.nextToken());
+
+//        // m개의 도로를 입력 (시작, 끝, t)
+//        List<Road>[] roads = new ArrayList[n + 1];
+//        for (int i = 1; i <= n; i++) {
+//            roads[i] = new ArrayList<>();
+//        }
+//
+//        for (int i = 0; i < m; i++) {
+//            st = new StringTokenizer(br.readLine(), " ");
+//            int start = Integer.parseInt(st.nextToken());
+//            int dest = Integer.parseInt(st.nextToken());
+//            int time = Integer.parseInt(st.nextToken());
+//
+//            roads[start].add(new Road(dest, time));
+//        }
+
+        int[][] distance = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == j) {
+                    distance[i][j] = 0;
+                    continue;
+                }
+                distance[i][j] = INF;
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            
+            int start = Integer.parseInt(st.nextToken());
+            int dest = Integer.parseInt(st.nextToken());
+            int time = Integer.parseInt(st.nextToken());
+            distance[start][dest] = Math.min(distance[start][dest], time);
+        }
+
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (distance[i][k] + distance[k][j] < distance[i][j]) {
+                        distance[i][j] = distance[i][k] + distance[k][j];
+                    }
+                }
+            }
+        }
+
+        int maxValue = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i != x) {
+                maxValue = Math.max(distance[i][x] + distance[x][i], maxValue);
+            }
+        }
+
+        System.out.println(maxValue);
+
+//        // 다익스트라로 (x에서 n으로 가는 경로 구하기) O(m)
+//        int[] comeback = dijkstra(x, n, roads);
+//
+//        // bfs로 n -> x로 가는 경로 구하기, O(n * nlogn)
+//        int[] go = new int[n+1];
+//        for (int i = 1; i <= n; i++){
+//            if (i != x){
+//                go[i] = calculateMinDistance(i, x);
+//            }
+//        }
+
+        // 둘을 더해서 최대인 값을 리턴
+    }
+
+//    private static int calculateMinDistance(int start, int dest){
+//        PriorityQueue<Road> pq = new PriorityQueue<>(new RoadComparator());
+//        pq.offer(new Road(start, 0));
+//
+//        while (true){
+//            Road road = pq.poll();
+//            if (road.dest == dest){
+//                return road.time;
+//            }
+//
+//            for (Road road: roads){
+//                pq.offer()
+//            }
+//
+//        }
+//    }
+
+    private static int[] dijkstra(int x, int n, List<Road>[] roads) {
+        int[] comeback = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            comeback[i] = INF;
+        }
+        comeback[x] = 0;
+
+        Deque<Road> queue = new ArrayDeque<>();
+        queue.offer(new Road(x, 0));
+
+        while (!queue.isEmpty()) {
+            Road current = queue.poll();
+
+            for (Road road : roads[current.dest]) {
+                if (current.time + road.time < comeback[road.dest]) {
+                    comeback[road.dest] = current.time + road.time;
+                    queue.offer(new Road(road.dest, comeback[road.dest]));
+                }
+            }
+        }
+        return comeback;
+    }
+
+    private static class Road {
+        private final int dest;
+        private final int time;
+
+        Road(int dest, int time) {
+            this.dest = dest;
+            this.time = time;
+        }
+    }
+}
+
+// n개의 마을, m개의 단방향 도로
+// 최단시간에 왔다가 가야함, 이때 최대로 걸리는 학생을 구하기
+
+//
+
+// 16:01 -
