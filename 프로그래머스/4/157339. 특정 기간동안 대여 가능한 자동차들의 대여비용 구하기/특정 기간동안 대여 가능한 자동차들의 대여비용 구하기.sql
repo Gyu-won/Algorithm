9faf5dec4dcd9b39b610fc18f23af8b2bed1fae0
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2024-03-02 11:19:57 +0900

    [level 4] Title: 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
+++ 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
@@ -1,35 +1,22 @@
 -- 코드를 입력하세요
 
-select car.car_id, car.car_type, 
-    floor(car.daily_fee * (100 - discount.discount_rate) / 100 * 30) as fee
-
-# 30일 할인 따로 구해서 join 하고
+select car.car_id, car.car_type, floor(30 * car.daily_fee * (100 - plan.discount_rate) / 100) as fee
 from car_rental_company_car car
-    join (select car_type, discount_rate
-         from car_rental_company_discount_plan
-         where duration_type = '30일 이상') discount
-    
-    on car.car_type = discount.car_type
-
-where car.car_type in ('세단', 'suv')
-
-    and
-    
-    car.car_id not in (
-        select car_id
-        from car_rental_company_rental_history
-        where date_format(start_date, '%Y-%m-%d') <= '2022-11-30'
-            and
-            date_format(end_date, '%Y-%m-%d') >= '2022-11-01'
-    )
-    
-    and car.daily_fee * (100 - discount.discount_rate) / 100 * 30 between 500000 and 1990000
+    join car_rental_company_discount_plan plan
+    on car.car_type = plan.car_type
+where plan.duration_type = '30일 이상'
+    and car.car_type in ('세단', 'SUV')
+    and car.car_id not in (select car_id
+                    from car_rental_company_rental_history
+                    where date_format(start_date, '%Y-%m-%d') <= '2022-11-30'
+                        and date_format(end_date, '%Y-%m-%d') >= '2022-11-01')
+    and 30 * car.daily_fee * (100 - plan.discount_rate) / 100 >= 500000
+    and 30 * car.daily_fee * (100 - plan.discount_rate) / 100 < 2000000
 
 order by fee desc, car.car_type, car.car_id desc;
-# 11월 1일 부터 11월 30일 대여 불가능한 차의 id를 뽑고
-# not in 하고, 자동차 종류가 'suv', '세단' 인거
 
-# 할인 금액 계산해서 정리
-    
 
-#10:05
+
+# 자동차 종ㄹ가 세단, suv
+# 20221101 - 20221130 대여 가능
+# 30일간 대역ㅁ액이 50만-200만 미마ㄴ
