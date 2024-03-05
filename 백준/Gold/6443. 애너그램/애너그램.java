2bc1037696143c2742aeb59966311b46c1c37811
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2024-03-05 12:43:33 +0900

    [Gold V] Title: 애너그램, Time: 436 ms, Memory: 61756 KB -BaekjoonHub

--- 백준/Gold/6443. 애너그램/애너그램.java
+++ 백준/Gold/6443. 애너그램/애너그램.java
@@ -1,25 +1,18 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 public class Main {
 
     private static int n;
     private static int[] alphabets = new int[26];
+    private static StringBuilder result = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         // n 을 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
 
-        Set<String> words = new HashSet<>();
-
-        StringBuilder result = new StringBuilder();
         for (int i = 0; i < n; i++) {
             // alphabets 입력
             char[] inputs = br.readLine().toCharArray();
@@ -28,20 +21,8 @@ public class Main {
             }
 
             // dfs
-            dfs(inputs.length, "", words);
+            dfs(inputs.length, "");
 
-            List<String> uniqueWords = new ArrayList<>(words);
-
-            // set 정렬
-            Collections.sort(uniqueWords);
-
-            // 출력
-            for (String word : uniqueWords) {
-                result.append(word);
-                result.append("\n");
-            }
-
-            words.clear();
             for (int j = 0; j < 26; j++) {
                 alphabets[j] = 0;
             }
@@ -50,16 +31,17 @@ public class Main {
 
     }
 
-    private static void dfs(int m, String word, Set<String> words) {
+    private static void dfs(int m, String word) {
         if (word.length() == m) {
-            words.add(word);
+            result.append(word);
+            result.append("\n");
             return;
         }
 
         for (int i = 0; i < alphabets.length; i++) {
             if (alphabets[i] > 0) {
                 alphabets[i]--;
-                dfs(m, word + (char) ('a' + i), words);
+                dfs(m, word + (char) ('a' + i));
                 alphabets[i]++;
             }
         }
