1b0a8c9e30711758e96becabc6f12ca4da7b6803
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2024-03-20 20:37:57 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 568 ms, Memory: 53040 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -4,50 +4,35 @@ import java.io.InputStreamReader;
 
 public class Main {
 
+    private static StringBuilder result = new StringBuilder();
+
     public static void main(String[] args) throws IOException {
         // n 입력 (1-20)
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
         // hanoi(n) 구하기
-        System.out.println(hanoiNumber(n));
-        System.out.println(hanoi(n).toString().trim());
+        System.out.println(hanoi(n, 1, 2, 3));
+        System.out.println(result.toString().trim());
     }
 
-    private static int hanoiNumber(int n) {
+    private static int hanoi(int n, int start, int mid, int end) {
         if (n == 1) {
+            result.append(start);
+            result.append(" ");
+            result.append(end);
+            result.append("\n");
             return 1;
         }
-        return hanoiNumber(n - 1) * 2 + 1;
-    }
 
-    private static StringBuilder hanoi(int n) {
-        if (n == 1) {
-            return new StringBuilder("1 3\n");
-        }
-        return stackTwo(hanoi(n - 1)).append("1 3\n").append(stackThree(hanoi(n - 1)));
-    }
-
-    private static StringBuilder stackTwo(StringBuilder hanoi) {
-        for (int i = 0; i < hanoi.length(); i++) {
-            if (hanoi.charAt(i) == '2') {
-                hanoi.setCharAt(i, '3');
-            } else if (hanoi.charAt(i) == '3') {
-                hanoi.setCharAt(i, '2');
-            }
-        }
-        return hanoi;
-    }
-
-    private static StringBuilder stackThree(StringBuilder hanoi) {
-        for (int i = 0; i < hanoi.length(); i++) {
-            if (hanoi.charAt(i) == '1') {
-                hanoi.setCharAt(i, '2');
-            } else if (hanoi.charAt(i) == '2') {
-                hanoi.setCharAt(i, '1');
-            }
-        }
-        return hanoi;
+        int count = hanoi(n - 1, start, end, mid);
+        result.append(start);
+        result.append(" ");
+        result.append(end);
+        result.append("\n");
+        count++;
+        count += hanoi(n - 1, mid, start, end);
+        return count;
     }
 }
 
