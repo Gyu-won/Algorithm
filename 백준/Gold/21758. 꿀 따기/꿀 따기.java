7bdafd2443599a4f52603336602edcfc5a4de818
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2024-04-02 12:46:17 +0900

    [Gold V] Title: 꿀 따기, Time: 316 ms, Memory: 25140 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/21758. 꿀 따기/꿀 따기.java
+++ 백준/Gold/21758. 꿀 따기/꿀 따기.java
@@ -6,69 +6,55 @@ import java.util.StringTokenizer;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        System.out.println(new Solution().solution());
-    }
+        // n 입력 (3-100,000)
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-    static class Solution {
-        public int solution() throws IOException {
-            // n을 입력받는다 (3-100000)
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            int n = Integer.parseInt(br.readLine());
-
-            // int[n] p 를 입력받는다.
-            int[] p = new int[n];
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            for (int i = 0; i < n; i++) {
-                p[i] = Integer.parseInt(st.nextToken());
-            }
-
-            int[] partSum = new int[n];
-            partSum[0] = p[0];
-            for (int i = 1; i < n; i++) {
-                partSum[i] = partSum[i - 1] + p[i];
-            }
-
-            int maxSum = 0;
-            for (int i = 0; i < n; i++) {
-                int sum = 0;
-                int lSum = partSum[i] - p[0];
-                int rSum = partSum[n - 2] - partSum[i] + p[i];
-                if (lSum < rSum) {
-                    sum += rSum;
-                    int rightSum = 0;
-                    for (int j = i + 1; j < n - 1; j++) {
-                        rightSum = Math.max(rightSum, partSum[j] - partSum[i] + p[i] - 2 * p[j]);
-                    }
-                    sum += Math.max(rightSum, lSum);
-                } else if (lSum > rSum) {
-                    sum += lSum;
-                    int leftSum = 0;
-                    for (int j = 1; j < i; j++) {
-                        leftSum = Math.max(leftSum, partSum[i] - partSum[j] - p[j]);
-                    }
-                    sum += Math.max(leftSum, rSum);
-                } else {
-                    sum += lSum * 2;
-                }
-                maxSum = Math.max(maxSum, sum);
-            }
-
-            return maxSum;
+        // honey 입력
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int[] honey = new int[n];
+        for (int i = 0; i < n; i++) {
+            honey[i] = Integer.parseInt(st.nextToken());
         }
+
+        System.out.println(calculateMaxHoney(n, honey));
     }
+
+    private static int calculateMaxHoney(int n, int[] honey) {
+        int[] cumulativeHoney = calculateCumulativeHoney(n, honey);
+        int left = 0, right = n - 1, maxHoney = 0;
+
+        for (int mid = left + 1; mid < right; mid++) {
+            maxHoney = Math.max(maxHoney, calculateHoney(left, mid, right, honey, cumulativeHoney));
+        }
+        return maxHoney;
+    }
+
+    private static int calculateHoney(int left, int mid, int right, int[] honey, int[] cumulativeHoney) {
+        int leftPot = cumulativeHoney[mid] + cumulativeHoney[right] - honey[right] - 2 * honey[mid];
+        int midPot = cumulativeHoney[right] - honey[left] - honey[right] + honey[mid];
+        int rightPot = 2 * cumulativeHoney[right] - cumulativeHoney[mid] - honey[left] - honey[mid];
+
+        return Math.max(leftPot, Math.max(midPot, rightPot));
+    }
+
+    private static int[] calculateCumulativeHoney(int n, int[] honey) {
+        int[] cumulative = new int[n];
+        cumulative[0] = honey[0];
+
+        for (int i = 1; i < n; i++) {
+            cumulative[i] = cumulative[i - 1] + honey[i];
+        }
+        return cumulative;
+    }
+
+
 }
 
-// 설계 시간: 00:40 - 00:58
-// 풀이 시간:
-
-//0. 문제요약
-// 좌우 n개의 장소 가 있다.
-// 2장소에는 벌을 한 장소에는 벌통을 둔다
-// 두 마리 벌이 날아가면서 꿀을 따는데 시작 장소 뺴고는 모두 표시된 숫자만큼 꿀을 딴다
-// 최대 꿀 따는 꿀 양 구해라
-
-//2. 시간복잡도:
-
-//3. 이 로직으로 예제 문제 손으로 풀기
-
-//4. 경계값 체크 후 코드 구현
+// O(nlogn)
+// 양 끝은 무조건 확정, 중간에 한개의 위치를 찾으면 됨 O(n)
+// 이 중 꿀통이 가장 왼쪽, 중간, 오른쪽 가능 O(3)
+// 계산은 누적합으로
+// 가장 왼쪽일때는 : cumulative[mid] + cumulative[right] - honey[right] - 2 * honey[mid]
+// 중간일때는: cumulative[mid] + cumulative[right] - cumulative[mid] - honey[left] - honey[right] + hone[mid]
+// 가장 오른쪽일떄는: 2 * cumulative[right] - cumulative[mid] - honey[left] - honey[mid]
