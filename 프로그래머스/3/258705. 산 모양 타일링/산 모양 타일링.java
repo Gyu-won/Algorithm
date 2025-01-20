0b7a5a723599d7c2c692f54f74cda3570e8c517d
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2025-01-20 10:58:44 +0900

    [level 3] Title: 산 모양 타일링, Time: 18.49 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
@@ -1,36 +1,33 @@
-import java.util.*;
-
 class Solution {
-
     private static final int DIVISOR = 10007;
-    public int solution(int n, int[] tops) {
-        int answer = 0;
-        
-        int[] effect = new int[n];
-        int[] noEffect = new int[n];
-        
-        effect[0] = 1;
-        noEffect[0] = 2;
-        if (tops[0] == 1) {
-            noEffect[0]++;
+    
+    public int solution(int n, int[] tops) {    
+        // dp 초기값 설정
+        int[][] dp = new int[n][2];
+        dp[0][0] = 1;
+        if (tops[0] == 0) {
+            dp[0][1] = 2;
+        } else{
+            dp[0][1] = 3;
         }
         
+        // tops 반복하며 dp[n-1] 까지 채우기 (% 10007)
         for (int i = 1; i < n; i++) {
-            effect[i] = (effect[i-1] + noEffect[i-1]) % DIVISOR;
+            dp[i][0] = dp[i-1][0] + dp[i-1][1];
             if (tops[i] == 0) {
-                noEffect[i] = effect[i-1] + 2 * noEffect[i-1];
+                dp[i][1] = dp[i-1][0] + 2 * dp[i-1][1];
             } else {
-                noEffect[i] = 2 * effect[i-1] + 3 * noEffect[i-1];
+                dp[i][1] = 2 * dp[i-1][0] + 3 * dp[i-1][1];
             }
-            noEffect[i] %= DIVISOR;
+            
+            dp[i][0] %= DIVISOR;
+            dp[i][1] %= DIVISOR;
         }
-        
-        return (effect[n-1] + noEffect[n-1]) % DIVISOR;
+        return (dp[n-1][0] + dp [n-1][1]) % DIVISOR;
     }
 }
 
-// dp 0 값 초기화
-// 2부터 n 까지 늘려가며 값 채우기
-// 경우의 수를 모두 나누어서 dp로 계산
+// 10:28
 
-// 16:20
+// 2차원 dp: O(n)
+// tops와 침범 여부에 따라 나누기
