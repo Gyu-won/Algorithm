8c0daf9730006ce588095c6a503d4f7dc2dec671
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2025-02-04 17:24:34 +0900

    [Silver II] Title: 초콜릿 뺏어 먹기, Time: 116 ms, Memory: 14444 KB -BaekjoonHub

--- 백준/Silver/23322. 초콜릿 뺏어 먹기/초콜릿 뺏어 먹기.java
+++ 백준/Silver/23322. 초콜릿 뺏어 먹기/초콜릿 뺏어 먹기.java
@@ -1,39 +1,41 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 class Main {
-	public static void main(String[] args) throws Exception {
-		// n, k 입력
+	public static void main(String[] args) throws IOException {
+		// n 입력
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
 		int n = Integer.parseInt(st.nextToken());
+
+		// k 입력
 		int k = Integer.parseInt(st.nextToken());
 
-		// int[n] chocolates 입력 (정렬 됨)
-		int[] chocolates = new int[n + 1];
+		// int chocolates[n] 입력
+		int[] chocolates = new int[n];
 		st = new StringTokenizer(br.readLine());
-		for (int i = 1; i <= n; i++) {
+		for (int i = 0; i < n; i++) {
 			chocolates[i] = Integer.parseInt(st.nextToken());
 		}
 
-		// O(n)
-		int day = 0, quantity = 0;
-		for (int i = 2; i <= n; i++) {
-			if (chocolates[1] < chocolates[i]) {
-				day++;
-				quantity += chocolates[i] - chocolates[1];
-				chocolates[i] = chocolates[1];
+		// n-1 순회하며 chocolate[0]과 다른 값을 더하고, chocolates[0] 빼기: O(n_
+		int chocolateToEat = 0;
+		int eatingDay = 0;
+		for (int i = 1; i < n; i++) {
+			int difference = chocolates[i] - chocolates[0];
+			if (difference > 0) {
+				chocolateToEat += difference;
+				eatingDay++;
 			}
 		}
 
-		System.out.printf("%d %d", quantity, day);
+		StringBuilder result = new StringBuilder();
+		result.append(chocolateToEat).append(" ").append(eatingDay);
+		System.out.println(result.toString());
 	}
 }
 
-// 초콜릿 오름차순 정렬되어 있음
-// K < i 인 i를 골라서 i - K 통에 있는 개수랑 똑같을 때까지 i 번째 통에서 초콜릿 먹고 재정렬
-// 최대한 많이 빨리 초콜릿 먹을 때 몇개 먹을지
+// 앞에것 부터 먹어서 빠르게 줄이
 
-// 가장 앞에 것 부터 고르는 게 가장 많이 빠르게 먹는 방법
