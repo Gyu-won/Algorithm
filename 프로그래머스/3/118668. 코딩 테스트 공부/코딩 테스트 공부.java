74c769c81e5a713b434ea17a3b97e6e75a97cdbd
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2025-03-20 11:48:18 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 66.09 ms, Memory: 61.1 MB -BaekjoonHub

--- 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.java
+++ 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.java
@@ -1,33 +1,95 @@
+import java.util.*;
 
 class Solution {
-        int maxAlp, maxCop;
-        int[][] problems;
-        Integer[][] DP;
+    public int solution(int alp, int cop, int[][] problems) {
+        List<Problem> problemList = new ArrayList<>();
+        int alpReqMax = 0;
+        int copReqMax = 0;
 
-        int 완전탐색(int alp, int cop) {
-            if (alp >= maxAlp && cop >= maxCop) return 0;
-            alp = Math.min(maxAlp, alp);
-            cop = Math.min(maxCop, cop);
-            if (DP[alp][cop] != null) return DP[alp][cop];
-            int cost = Integer.MAX_VALUE;
-            if (alp < maxAlp) cost = Math.min(cost, 1 + 완전탐색(alp + 1, cop));
-            if (cop < maxCop) cost = Math.min(cost, 1 + 완전탐색(alp, cop + 1));
-            for (int[] p : problems)
-                if (p[0] <= alp && p[1] <= cop) {
-                    if ((alp >= maxAlp && p[3] == 0) || (cop >= maxCop && p[2] == 0)) continue;
-                    cost = Math.min(cost, p[4] + 완전탐색(alp + p[2], cop + p[3]));
-                }
-            return DP[alp][cop] = cost;
+        problemList.add(new Problem(0, 0, 1, 0, 1));
+        problemList.add(new Problem(0, 0, 0, 1, 1));
+
+        for (int i = 0; i < problems.length; i++) {
+            int[] problem = problems[i];
+            alpReqMax = Math.max(alpReqMax, problem[0]);
+            copReqMax = Math.max(copReqMax, problem[1]);
+            problemList.add(new Problem(problem[0], problem[1], problem[2], problem[3], problem[4]));
+        }
+        Collections.sort(problemList);
+
+        PriorityQueue<Power> pq = new PriorityQueue<>();
+        int[][] isVisited = new int[1000][1000];
+        for(int i = 0; i < 1000; i++) {
+            Arrays.fill(isVisited[i], Integer.MAX_VALUE);
         }
 
-        public int solution(int alp, int cop, int[][] problems) {
-            this.problems = problems;
-            maxAlp = maxCop = 0;
-            for (int[] p : problems) {
-                maxAlp = Math.max(maxAlp, p[0]);
-                maxCop = Math.max(maxCop, p[1]);
+        isVisited[alp][cop] = 0;
+        pq.add(new Power(alp, cop, 0));
+
+        int answer = 0;
+        while (!pq.isEmpty()) {
+            Power p = pq.poll();
+            int nowAlp = p.alp;
+            int nowCop = p.cop;
+            int time = p.time;
+
+            if(isVisited[nowAlp][nowCop] < time) continue;
+            if (alpReqMax <= nowAlp && copReqMax <= nowCop) {
+                answer = time;
+                break;
             }
-            DP = new Integer[maxAlp + 1][maxCop + 1];
-            return 완전탐색(alp, cop);
+
+            for (Problem pr : problemList) {
+                int alpReq = pr.alpReq;
+                int copReq = pr.copReq;
+                int nextAlp = nowAlp + pr.alpRwd;
+                int nextCop = nowCop + pr.copRwd;
+                int nextCost = time + pr.cost;
+
+                if (nowAlp >= alpReq && nowCop >= copReq && isVisited[nextAlp][nextCop] > nextCost) {
+                    isVisited[nextAlp][nextCop] = nextCost;
+                    pq.add(new Power(nextAlp, nextCop, nextCost));
+                }
+            }
         }
+
+        return answer;
     }
+}
+class Power implements Comparable<Power> {
+    int alp;
+    int cop;
+    int time;
+
+    public Power(int alp, int cop, int time) {
+        this.alp = alp;
+        this.cop = cop;
+        this.time = time;
+    }
+
+    @Override
+    public int compareTo(Power p) {
+        return Integer.compare(this.time, p.time);
+    }
+}
+
+class Problem implements Comparable<Problem> {
+    int alpReq;
+    int copReq;
+    int alpRwd;
+    int copRwd;
+    int cost;
+
+    public Problem(int alpReq, int copReq, int alpRwd, int copRwd, int cost) {
+        this.alpReq = alpReq;
+        this.copReq = copReq;
+        this.alpRwd = alpRwd;
+        this.copRwd = copRwd;
+        this.cost = cost;
+    }
+
+    @Override
+    public int compareTo(Problem p) {
+        return Integer.compare(this.cost, p.cost);
+    }
+}
