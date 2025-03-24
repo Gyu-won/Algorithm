272e0c2e57d42b088fc2010ee6feaf16b06232fc
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2025-03-24 11:10:06 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 4.91 ms, Memory: 85.8 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -1,24 +1,25 @@
+
 import java.util.*;
 
 class Solution {
     // s 길이 1~15*10^4
     public int[] solution(String s) {
         int[] answer = new int[] {0, 0};
-        
+
         while(!s.equals("1")) {
             answer[0]++;
-            int prevLength = s.length();
-            s = s.replaceAll("0", "");
-            answer[1] += prevLength - s.length();
-            s = Integer.toString(s.length(), 2);
+
+            int oneCount = 0;
+            for (char ch: s.toCharArray()) {
+                if (ch == '1') {
+                    oneCount++;
+                }
+            }
+
+            answer[1] += s.length() - oneCount;
+            s = Integer.toString(oneCount, 2);
         }
-        
+
         return answer;
     }
 }
-
-// 이진 변환: s가 "1"될 때까지
-// 1개수 구하기 (하나씩 보면서 탐색)
-// count++;
-// 0 개수 반영
-// s 변경
